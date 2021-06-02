0b91542ccfb655e0a67a6d5fa486276091502250
SWoolman <sandy@woolmans.net>
2021-06-02 17:10:28 +0200

    did some stuff

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Bullet : MonoBehaviour
+{
+    //Handle the bullets
+    
+    [SerializeField] private GameObject hitEffect;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 0.2f);
+        Destroy(gameObject);
+       // FindObjectOfType<AudioManager>().Play("Boom");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 0.2f);
+        Destroy(gameObject);
+        //FindObjectOfType<AudioManager>().Play("Boom");
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+}
