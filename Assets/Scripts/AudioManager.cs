852d98c4bfd0a30be5ead64dc130ad3b9fbddd1c
SWoolman <sandy@woolmans.net>
2021-06-25 18:27:19 +0200

    Added sounds and music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    //handle sounds and music
+
+    [SerializeField] private Sound[] sounds;
+
+    private static AudioManager instance_;
+    void Awake()
+    {
+        if (instance_ == null)
+            instance_ = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        DontDestroyOnLoad(gameObject);
+        
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }    
+    }
+
+    private void Start()
+    {
+        Play("Music");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
